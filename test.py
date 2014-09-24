380d12d148bb98e8d432533da8482f52c3c36cae
nbrennan0317 <nbrennan@gmail.com>
2014-09-23 21:04:37 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,28 @@
-if __name__ == "__main__":
-    import wink
+  # for device in w.device_list():
+   #     print device.device_type(), device.id
+   # for service in w.get_services():
+   #     print service
+   # print "-- - - - - ROBOTS - - - - --"
+   # for robots in w.get_robots():
+   #     print robots
+   # print w.get_robots()
 
-    w = wink.init()
+  #  print w.get_profile()
+   # for devi in w.get_devices():
+   #     print " -------- "
+   #     print devi
 
-    for device in w.device_list():
-        print device.device_type(), device.id
+
+
+   # print " ---------- test ----------"
+   # print w.get_test()
+
+
+    #print " ---------- Cantina ----------"
+   # print w.cantina_on()
+
+    #print w.get_scenes()
+    #print w.can_test()
+
+    #print json.dumps(w._get('/light_bulbs/14952/users'))
+    print json.dumps(w._get('/remotes/2175/users'))
